062b63db3a1de3f5ae375a01cc545bb07e0acd65
wy <haveaballmyball@hotmail.com>
2024-01-13 18:51:40 +0800

    feat:粗版main.js

--- main.js
+++ main.js
@@ -1,9 +1,49 @@
-// 1. 先按原生的写法生成 app 文本
-// ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+function createElement(el, props, ...children) {
+  const dom = document.createElement(el);
 
-const dom = document.createElement("div");
-dom.id = "app";
-document.body.appendChild(dom);
+  Object.keys(props).forEach((key) => {
+    dom[key] = props[key];
+  });
 
-const textElement = document.createTextNode("app");
-dom.appendChild(textElement);
+  children.forEach((child) => {
+    if (typeof child === "string") {
+      return dom.appendChild(document.createTextNode(child));
+    }
+    const elChild = document.createTextNode("");
+    Object.keys(child.props).forEach((son) => {
+      elChild[son] = child.props[son];
+    });
+    dom.appendChild(elChild);
+  });
+  return dom;
+}
+
+const App = createElement("div", { id: "app" }, "hello", "-", "mini-react");
+
+function render(App, container) {
+  return container.appendChild(App);
+}
+
+const ReactDOM = {
+  createRoot(container) {
+    return {
+      render(App) {
+        render(App, container);
+      },
+    };
+  },
+};
+
+ReactDOM.createRoot(document.querySelector("#root")).render(App);
+
+// document.querySelector("#root").appendChild(createElement("div", { id: "app" }, "string", "hello"));
+// document
+//   .querySelector("#root")
+//   .appendChild(
+//     createElement(
+//       "div",
+//       { id: "app" },
+//       { type: "TEXT_ELEMENT", props: { nodeValue: "mini", children: [] } },
+//       { type: "TEXT_ELEMENT", props: { nodeValue: "hello", children: [] } }
+//     )
+//   );
