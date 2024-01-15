6d810cbbffccb802cc473eac50fc4f7469a441e8
wy <haveaballmyball@hotmail.com>
2024-01-15 10:45:08 +0800

    fix:修改部分变量名

--- vite-runner/core/React.js
+++ vite-runner/core/React.js
@@ -88,26 +88,26 @@ function initChildren() {
   // 遍历 div 的 props.children 数组
   children.forEach((child, index) => {
     // 这里新建一个对象是为了获取每一个元素的父级元素，同时不破坏原先设置的 vdom 的数据结构
-    const newChild = {
+    const newFiber = {
       type: child.type,
       props: child.props,
       child: null,
       parent: fiber,
-      brother: null,
+      sibling: null,
       dom: null,
     };
     if (index === 0) {
       // 当第一个数据进来的时候, 这个赋值给 fiber.child
       // 当第一个 span 进来的时候，将这个数据赋值给 prevChild
-      fiber.child = newChild;
+      fiber.child = newFiber;
     } else {
-      // 当第二个 span 进来的时候，因为 javascript 对象引用的是地址，而非真实对象，所以当把 preChild[brother]赋值的时候，同样也是给 fiber.child[brother]赋值，所以 fiber 的 child 里会有 brother 属性
-      prevChild.brother = newChild;
+      // 当第二个 span 进来的时候，因为 javascript 对象引用的是地址，而非真实对象，所以当把 preChild[sibling]赋值的时候，同样也是给 fiber.child[sibling]赋值，所以 fiber 的 child 里会有 sibling 属性
+      prevChild.sibling = newFiber;
     }
     // 然后再将这个数据保持下来
     // 第二次将下标为 0 的 span 里的数据赋值给 prevChild
     // 第三次将下标为 1 的 span 里的数据赋值给 prevChild
-    prevChild = newChild;
+    prevChild = newFiber;
   });
 }
 
@@ -124,10 +124,10 @@ function performUnitOfWork(fiber) {
   if (fiber.child) {
     return fiber.child;
   }
-  if (fiber.brother) {
-    return fiber.brother;
+  if (fiber.sibling) {
+    return fiber.sibling;
   }
-  return fiber.parent?.brother;
+  return fiber.parent?.sibling;
 }
 function workLoop(deadline) {
   while (!shouldYield && fiber) {
